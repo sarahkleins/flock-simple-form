158b05d4dc26d9c5caa083741ebef6d9ea0e1f22
sarahkleins <kleinman.sarah@gmail.com>
2017-09-21 13:47:14 -0600

    Misc updates to tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
 import App from '../App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('Component: App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  it('renders h1', () => {
+    const wrapper = shallow(<App />);
+    const h1 = <h1>Simple Form</h1>;
+    expect(wrapper.contains(h1)).toEqual(true);
+  });
 });

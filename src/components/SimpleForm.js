26fe2db5f3edf6fde1eb30a4210ff91d0c9171ed
sarahkleins <kleinman.sarah@gmail.com>
2017-09-21 11:47:03 -0600

    Add SimpleForm component scaffold

---
+++ src/components/SimpleForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SimpleForm extends Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
+      <form></form>
+    );
+  }
+}
+
+export default SimpleForm;

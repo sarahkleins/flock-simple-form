91370ec0c09470b651c961d4120d3eb6b0e16725
sarahkleins <kleinman.sarah@gmail.com>
2017-09-21 11:52:15 -0600

    Add initial form state and labels/input fields

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -3,11 +3,39 @@ import React, { Component } from 'react';
 class SimpleForm extends Component {
   constructor(){
     super();
+
+    this.state = {
+      userEmail: '',
+      userPassword: '',
+      userPasswordConfirmation: ''
+    }
   }
 
   render(){
     return(
-      <form></form>
+      <form>
+        <label>
+          Email:
+          <input
+            name="userEmail"
+            type="text"
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            name="userPassword"
+            type="password"
+          />
+        </label>
+        <label>
+          Confirm Password:
+          <input
+            name="userPasswordConfirmation"
+            type="password"
+          />
+        </label>
+      </form>
     );
   }
 }
